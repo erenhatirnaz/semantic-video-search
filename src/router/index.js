488a19975104b92212462649290eafcd191a3eff
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2018-01-30 01:37:44 +0300

    Adds search page to router config

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import MainPage from '@/components/MainPage'
+import SearchPage from '@/components/SearchPage'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'MainPage',
       component: MainPage
+    },
+    {
+      path: '/search',
+      name: 'SearchPage',
+      component: SearchPage
     }
   ]
 })
